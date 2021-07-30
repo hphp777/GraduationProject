b070a8123d9ff46db9f1aff5a6cc5bccff5b659d
hphp777 <85219925+hphp777@users.noreply.github.com>
2021-07-30 19:57:12 +0900

    message

--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.shortcuts import reverse
 from django.db import models
 
 # Create your models here.
@@ -28,3 +29,6 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+    def get_absolute_url(self):
+        return reverse('users:profile', kwargs={'pk': self.pk})
--- users/views.py
+++ users/views.py
@@ -1,11 +1,13 @@
-from django.views.generic import FormView
+from django.views.generic import FormView, DetailView
 from django.urls import reverse_lazy
 from django.shortcuts import redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib import auth
+from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
+from . import models
 
 from .forms import UserForm
 # Create your views here.
@@ -26,6 +28,7 @@ def login(request):
         return render(request, 'login.html')
 
 def log_out(request):
+    messages.info(request, f"See you later")
     logout(request)
     return redirect(reverse_lazy("core:home"))
 
@@ -39,3 +42,14 @@ def signup(request):
     else:
         form = UserForm()
     return render(request, 'signup.html', {'form': form})
+
+class UserProfileView(DetailView):
+    
+    model = models.User
+
+    # template안에 더 많은 context를 사용할 수 있게 해 줄것이다.
+    # super를 의무적으로 호출해야 하는데 이유는 user_obj를 기본으로 주기 때문에
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["hello"] = "Hello!"
+        return context
