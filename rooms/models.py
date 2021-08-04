174acb63009036c9c983d13ec4a2b871c89bc109
hphp777 <85219925+hphp777@users.noreply.github.com>
2021-08-04 20:42:51 +0900

    edit photo

--- rooms/models.py
+++ rooms/models.py
@@ -128,12 +128,6 @@ class Room(core_models.TimeStampedModel):
         print(photos)
         return photos
 
-    def get_beds(self):
-        if self.beds == 1:
-            return "1 bed"
-        else:
-            return f"{self.beds} beds"
-
 class Photo (core_models.TimeStampedModel):
 
     """ Photo Model Definition """
